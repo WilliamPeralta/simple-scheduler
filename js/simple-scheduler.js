ca11e45a155dc1e36a96aea43566f2040825a0ed
Administrator <william.peralta18@gmail.com>
2015-09-22 00:02:00 +0200

    render body

--- js/simple-scheduler.js
+++ js/simple-scheduler.js
@@ -6,6 +6,7 @@ sScheduler = function(selector,options){
     };
     var renderTitles = function(){
         var html = '<thead><tr>';
+        html += '<th></th>';
         $.each(options.titles,function(k,v){
             html += '<th>'+ v.title+'</th>';
         });
@@ -13,7 +14,30 @@ sScheduler = function(selector,options){
         return html;
     };
     var renderBody = function(){
-        return "";
+        var html = '<tbody>';
+        var intervals = getIntervals();
+        for(var i = 0;i<intervals.length;i++){
+            html += '<tr>';
+            html += '<td><small>'+intervals[i].from.format("HH:mm")+" - "+intervals[i].to.format("HH:mm")+'</small></td>';
+            $.each(options.titles,function(k,v){
+                html += '<td>'+ v.title+'</td>';
+            });
+            html += '</tr>';
+        }
+        html += '</tbody>';
+        return html;
+    }
+    var getIntervals=function(){
+        var intervals = [];
+        $.each(options.orari,function(k,v){
+            for (var start = moment(v.from,"HH:mm");start.isBefore(moment(v.to,"HH:mm"));start.add(options.celInterval,'minutes')){
+                var from = moment(start);
+                var to = moment(start).add(options.celInterval,'minutes');
+                console.log(from.format("HH:mm")+" "+to.format("HH:mm"));
+                intervals.push({from:from,to:to});
+            }
+        });
+        return intervals;
     }
     render();
 };
