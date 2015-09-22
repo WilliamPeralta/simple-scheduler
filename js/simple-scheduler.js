f60d9a568f80988bbd7bad7c699d19f4c4381a08
Administrator <william.peralta18@gmail.com>
2015-09-23 00:54:36 +0200

    drag & drop events with jquery ui

--- js/simple-scheduler.js
+++ js/simple-scheduler.js
@@ -1,11 +1,18 @@
 
 sScheduler = function(selector,options){
+    var self = this;
+    this.currentDay = moment(options.startDate,"YYYY-MM-DD");
+    var dateTimeFormat = "YYYY-MM-DD HH:mm";
     var render = function(){
-        var html = '<table class="table table-striped">'+renderTitles()+renderBody()+'</table>';
+        var html = '<table class="table table-striped sscheduler">'+renderTitles()+renderBody()+'</table>';
         $(selector).html(html);
-        $( "#selectable" ).selectable({
-            filter:"td"
-        });
+        /*$( "#selectable" ).selectable({
+            filter:"td.event-container"
+        });*/
+        getEvents();
+        $(".event-cel").height(options.celHeight);
+
+
     };
     var renderTitles = function(){
         var html = '<thead><tr>';
@@ -23,7 +30,7 @@ sScheduler = function(selector,options){
             html += '<tr>';
             html += '<td><small>'+intervals[i].from.format("HH:mm")+" - "+intervals[i].to.format("HH:mm")+'</small></td>';
             $.each(options.titles,function(k,v){
-                html += '<td>'+ v.title+'</td>';
+                html += '<td class="event-container" data-key="'+ v.key+'" data-from="'+intervals[i].from.format(dateTimeFormat)+'" data-to="'+intervals[i].to.format(dateTimeFormat)+'" ><div class="event-cel">'+ v.title+'</div></td>';
             });
             html += '</tr>';
         }
@@ -37,10 +44,56 @@ sScheduler = function(selector,options){
                 var from = moment(start);
                 var to = moment(start).add(options.celInterval,'minutes');
                 console.log(from.format("HH:mm")+" "+to.format("HH:mm"));
-                intervals.push({from:from,to:to});
+                intervals.push({from:from.set({year:self.currentDay.get("year"),month:self.currentDay.get("month"),date:self.currentDay.get("date")}),to:to.set({year:self.currentDay.get("year"),month:self.currentDay.get("month"),date:self.currentDay.get("date")})});
             }
         });
         return intervals;
     }
-    render();
+    var getEvents = function(){
+        var request = {from:"2015-09-21 08:00",to:"2015-09-21 22:00"};
+        options.source(request,function(events){
+            //console.log(events);
+            for(var i=0;i<events.length;i++){
+                addEvent(events[i]);
+            }
+            $( ".event" ).draggable();
+            $( ".event-cel" ).droppable({
+                accept: ".event",
+                activeClass: "ui-state-hover",
+                hoverClass: "ui-state-active",
+                drop: function( event, ui ) {
+                    var parent = $(ui.draggable).parent().get(0);
+                    if(this!=parent){
+                        if($(".event",$(this)).length==0)
+                            $(this).html(ui.draggable);
+                    }
+                    $(ui.draggable).css({top:0,left:0})
+                }
+            });
+        });
+    }
+    var addEvent = function(event){
+        //prendo tutti td con la key del evento
+        var elementsKey = $(".event-container[data-key="+event[options.keyName]+"]");
+        //comparo la data d'inizio
+        elementsKey.each(function(){
+            var $this = $(this);
+            var mfrom = moment($this.data("from"),dateTimeFormat);
+            var mto = moment($this.data("to"),dateTimeFormat);
+            var mstart =moment(event.start);
+            var mend =moment(event.end);
+            if(mstart.isBetween(mfrom,mto)||mstart.isSame(mfrom)){
+                $this.find(".event-cel").html("<div class='event' style='height: "+(mend.diff(mstart,"minutes"))+"px'><div class='event-draw'>"+event.title+"</div></div>");
+                return false;
+            }else{
+                console.log(mstart.format(dateTimeFormat)+" "+mfrom.format(dateTimeFormat));
+            }
+        });
+    }
+    var init=function(){
+
+        render();
+    }
+    init();
+
 };
